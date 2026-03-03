773ae8b2c379612ecae3e0638417584e243d10d7
Rithish Barath <rithishbarath@Rithishs-MacBook-Air-6.local>
2026-03-03 10:35:00 +0530

    UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,18 @@
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" ********  ********  ********  ******** ");
+        
+        System.out.println("*        *        *  *       *        ");
+        
+        System.out.println("*        *        *  *       *        ");
+        
+        System.out.println("*        *        *  ******** *******  ");
+        
+        System.out.println("*        *        *  *        *       ");
+        
+        System.out.println("*        *        *  *        *       ");
+        
+        System.out.println(" ********  ********  *        ******** ");
     }
 }
-
-
